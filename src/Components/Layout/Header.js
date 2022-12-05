2e5c2d0d413091ac54f2900e044a8c6fc05dcc74
adithya-yadav <badriadithya7396@gmail.com>
2022-12-05 08:44:15 +0530

    add_summary

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import mealsImg from "../../mealsImg/meals.jpeg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCardButton";
+import classSum from "./MealsSummary.module.css"
 
 const Header = (props) => {
   return (
@@ -10,9 +11,19 @@ const Header = (props) => {
         ReactMeals
         <HeaderCartButton />
       </header>
-
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt={"A table full of meals"} />
+      </div>
+      <div className={classSum.summary}>
+        <h2>Delicious Food, Delivered To You</h2>
+        <p>
+          Choose your favourite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dunner at home.
+        </p>
+        <p>
+          All our meals are cooked with high-quality ingredients, just-in-time
+          and of course by experienced chefs.
+        </p>
       </div>
     </Fragment>
   );
